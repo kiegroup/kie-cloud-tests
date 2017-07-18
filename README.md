62a338c9dbc089481524924b05fda1bdd7a449f4
Karel Suta <ksuta@redhat.com>
2017-07-18 10:25:01 +0200

    Basic Kie cloud test coverage

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # kie-cloud-tests
 Cloud (OpenShift) integration tests for KIE projects.
+
+Tests currently cover just Kie deployments deployed on OpenShift.
+
+## Test execution
+
+How to run the tests on OpenShift:
+1. Start OpenShift - You can use any OpenShift instance, for example OpenShift started by [oc cluster up](https://github.com/openshift/origin/blob/master/docs/cluster_up_down.md)
+1. Prepare your GIT provider (for example install and start GitLab). You can skip this point and use GitHub account.
+1. Run tests using the command ```mvn clean install -Popenshift <specific-params>```
+
+The following table lists all currently required and supported parameters:
+
+### OpenShift properties
+
+| \<specific-params\>  | Default value  |  Meaning                                              |
+| -------------------- | ------ | ------------------------------------------------------------- |
+| openshift.master.url |        | URL pointing to OpenShift, for example https://127.0.0.1:8443 |
+| openshift.username   |  user  | Username for logging into OpenShift                           |
+| openshift.password   | redhat | Password for logging into OpenShift                           |
+| kie.image.streams    |        | URL pointing to file with image stream definitions            |
+
+### GIT provider properties
+
+Chooose one GIT provider
+
+#### GitLab
+
+| \<specific-params\> | Value  |  Meaning                              |
+| ------------------- | ------ | ------------------------------------- |
+| git.provider        | GitLab |                                       |
+| gitlab.url          |        | Url pointing to GitLab instance       |
+| gitlab.username     |        | Username for logging into GitLab      |
+| gitlab.password     |        | Password for logging into GitLab      |
+
+#### GitHub
+
+| \<specific-params\> | Value  |  Meaning                              |
+| ------------------- | ------ | ------------------------------------- |
+| git.provider        | GitHub |                                       |
+| github.username     |        | Username for logging into GitHub      |
+| github.password     |        | Password for logging into GitHub      |
---
+++ test-cloud/test-cloud-remote/src/test/java/org/kie/cloud/integrationtests/smoke/MultipleProcessesIntegrationTest.java
@@ -0,0 +1,111 @@
+package org.kie.cloud.integrationtests.smoke;
+
+import java.util.List;
+
+import org.assertj.core.api.Assertions;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.kie.cloud.api.DeploymentScenarioBuilderFactory;
+import org.kie.cloud.api.scenario.WorkbenchWithKieServerScenario;
+import org.kie.cloud.common.provider.KieServerClientProvider;
+import org.kie.cloud.common.provider.WorkbenchClientProvider;
+import org.kie.cloud.git.GitProvider;
+import org.kie.cloud.git.GitProviderFactory;
+import org.kie.server.api.model.KieContainerStatus;
+import org.kie.server.api.model.KieServerInfo;
+import org.kie.server.api.model.instance.ProcessInstance;
+import org.kie.server.api.model.instance.TaskSummary;
+import org.kie.server.integrationtests.controller.client.KieServerMgmtControllerClient;
+
+public class MultipleProcessesIntegrationTest {
+
+    private static final String ORGANIZATION_UNIT_NAME = "myOrgUnit";
+    private static final String REPOSITORY_NAME = "myRepo";
+
+    private static final String PROJECT_GROUP_ID = "org.kie.server.testing";
+    private static final String PROJECT_NAME = "definition-project";
+    private static final String PROJECT_VERSION = "1.0.0.Final";
+
+    private static final String CONTAINER_ID = "cont-id";
+    private static final String CONTAINER_ALIAS = "cont-alias";
+
+    private static final String GIT_REPOSITORY_NAME = "myGitRepo";
+
+    private static final String USERTASK_PROCESS_ID = "definition-project.usertask";
+    private static final String SIGNALTASK_PROCESS_ID = "definition-project.signaltask";
+
+    private static final String USER_YODA = "yoda";
+
+    private static final String SIGNAL_NAME = "signal1";
+
+    private static DeploymentScenarioBuilderFactory deploymentScenarioFactory = DeploymentScenarioBuilderFactory.getInstance();
+    private WorkbenchWithKieServerScenario workbenchWithKieServerScenario;
+    private WorkbenchClientProvider workbenchClientProvider;
+    private KieServerClientProvider kieServerClientProvider;
+    private KieServerMgmtControllerClient kieControllerClient;
+    private GitProvider gitProvider;
+
+    @Before
+    public void setUp() {
+        workbenchWithKieServerScenario = deploymentScenarioFactory.getWorkbenchWithKieServerScenarioBuilder().build();
+        workbenchWithKieServerScenario.deploy();
+
+        gitProvider = GitProviderFactory.getGitProvider();
+        gitProvider.createGitRepository(GIT_REPOSITORY_NAME, ClassLoader.class.getResource("/kjars-sources").getFile());
+
+        workbenchClientProvider = new WorkbenchClientProvider(workbenchWithKieServerScenario.getWorkbenchDeployment());
+        workbenchClientProvider.createOrganizationalUnit(ORGANIZATION_UNIT_NAME, workbenchWithKieServerScenario.getWorkbenchDeployment().getUsername());
+        workbenchClientProvider.cloneRepository(ORGANIZATION_UNIT_NAME, REPOSITORY_NAME, gitProvider.getRepositoryUrl(GIT_REPOSITORY_NAME));
+        workbenchClientProvider.deployProject(REPOSITORY_NAME, PROJECT_NAME);
+
+        kieServerClientProvider = new KieServerClientProvider(workbenchWithKieServerScenario.getKieServerDeployment());
+        kieControllerClient = new KieServerMgmtControllerClient(workbenchWithKieServerScenario.getWorkbenchDeployment().getUrl().toString() + "/rest/controller",
+                workbenchWithKieServerScenario.getWorkbenchDeployment().getUsername(), workbenchWithKieServerScenario.getWorkbenchDeployment().getPassword());
+    }
+
+    @After
+    public void tearDown() {
+        workbenchWithKieServerScenario.undeploy();
+        gitProvider.deleteGitRepository(GIT_REPOSITORY_NAME);
+    }
+
+    @Test
+    public void testMultipleDifferentProcessesOnSameKieServer() {
+        KieServerInfo serverInfo = kieServerClientProvider.getKieServerClient().getServerInfo().getResult();
+        kieControllerClient.saveContainerSpec(serverInfo.getServerId(), serverInfo.getName(), CONTAINER_ID, CONTAINER_ALIAS, PROJECT_GROUP_ID, PROJECT_NAME, PROJECT_VERSION, KieContainerStatus.STARTED);
+
+        kieServerClientProvider.waitForContainerStart(CONTAINER_ID);
+
+        Long userTaskPid = kieServerClientProvider.getProcessClient().startProcess(CONTAINER_ID, USERTASK_PROCESS_ID);
+        Assertions.assertThat(userTaskPid).isNotNull();
+        Long signalTaskPid = kieServerClientProvider.getProcessClient().startProcess(CONTAINER_ID, SIGNALTASK_PROCESS_ID);
+        Assertions.assertThat(signalTaskPid).isNotNull();
+
+        finishUserTaskProcess();
+        finishSignalTaskProcess(signalTaskPid);
+
+        ProcessInstance userTaskPi = kieServerClientProvider.getProcessClient().getProcessInstance(CONTAINER_ID, userTaskPid);
+        Assertions.assertThat(userTaskPi).isNotNull();
+        Assertions.assertThat(userTaskPi.getState()).isEqualTo(org.kie.api.runtime.process.ProcessInstance.STATE_COMPLETED);
+
+        ProcessInstance signalTaskPi = kieServerClientProvider.getProcessClient().getProcessInstance(CONTAINER_ID, signalTaskPid);
+        Assertions.assertThat(signalTaskPi).isNotNull();
+        Assertions.assertThat(signalTaskPi.getState()).isEqualTo(org.kie.api.runtime.process.ProcessInstance.STATE_COMPLETED);
+    }
+
+    private void finishUserTaskProcess() {
+        List<TaskSummary> tasks = kieServerClientProvider.getTaskClient().findTasks(USER_YODA, 0, 10);
+        Assertions.assertThat(tasks).isNotNull().hasSize(1);
+
+        kieServerClientProvider.getTaskClient().completeAutoProgress(CONTAINER_ID, tasks.get(0).getId(), USER_YODA, null);
+    }
+
+    private void finishSignalTaskProcess(Long pid) {
+        List<String> signals = kieServerClientProvider.getProcessClient().getAvailableSignals(CONTAINER_ID, pid);
+        Assertions.assertThat(signals).isNotNull().hasSize(1);
+        Assertions.assertThat(signals.get(0)).isEqualTo(SIGNAL_NAME);
+
+        kieServerClientProvider.getProcessClient().signal(CONTAINER_ID, SIGNAL_NAME, null);
+    }
+}
