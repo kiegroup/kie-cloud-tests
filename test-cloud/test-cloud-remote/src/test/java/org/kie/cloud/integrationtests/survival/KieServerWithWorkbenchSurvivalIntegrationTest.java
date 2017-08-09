8c1da19706fee73f79a0823e3a63a101ef372e97
Jakub Schwan <jschwan@redhat.com>
2017-08-09 09:28:51 +0200

    [GUVNOR-3339] Implement tests which scale Kie server to zero in Openshift

---
+++ test-cloud/test-cloud-remote/src/test/java/org/kie/cloud/integrationtests/survival/KieServerWithWorkbenchSurvivalIntegrationTest.java
@@ -0,0 +1,157 @@
+/*
+ * Copyright 2017 JBoss by Red Hat.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kie.cloud.integrationtests.survival;
+
+import java.util.concurrent.TimeUnit;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
+
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
+import org.junit.Before;
+import org.junit.Test;
+import org.kie.cloud.api.DeploymentScenarioBuilderFactory;
+import org.kie.cloud.api.scenario.WorkbenchWithKieServerScenario;
+import org.kie.cloud.common.provider.KieServerClientProvider;
+import org.kie.cloud.common.provider.KieServerControllerClientProvider;
+import org.kie.cloud.integrationtests.AbstractCloudIntegrationTest;
+import org.kie.cloud.integrationtests.util.WorkbenchUtils;
+import org.kie.server.api.model.KieContainerStatus;
+import org.kie.server.api.model.KieServerInfo;
+import org.kie.server.api.model.instance.ProcessInstance;
+import org.kie.server.client.KieServicesClient;
+import org.kie.server.client.ProcessServicesClient;
+import org.kie.server.client.QueryServicesClient;
+import org.kie.server.controller.api.model.spec.ServerTemplate;
+import org.kie.server.integrationtests.controller.client.KieServerMgmtControllerClient;
+import org.kie.server.integrationtests.shared.filter.Authenticator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class KieServerWithWorkbenchSurvivalIntegrationTest extends AbstractCloudIntegrationTest<WorkbenchWithKieServerScenario> {
+
+    private static final String PROJECT_GROUP_ID = "org.kie.server.testing";
+    private static final String PROJECT_NAME = "definition-project";
+    private static final String PROJECT_VERSION = "1.0.0.Final";
+
+    private static final String CONTAINER_ID = "cont-id";
+    private static final String CONTAINER_ALIAS = "cont-alias";
+
+    private static final String SIGNALTASK_PROCESS_ID = "definition-project.signaltask";
+
+    private static final String GET_CONTAINERS_REST_ENDPOINT = "/services/rest/server/containers";
+
+    private KieServerMgmtControllerClient kieServerMgmtControllerClient;
+
+    protected KieServicesClient kieServicesClient;
+    protected ProcessServicesClient processServicesClient;
+    protected QueryServicesClient queryServicesClient;
+
+    private static final Logger logger = LoggerFactory.getLogger(KieServerWithWorkbenchSurvivalIntegrationTest.class);
+
+    @Override
+    protected WorkbenchWithKieServerScenario createDeploymentScenario(DeploymentScenarioBuilderFactory deploymentScenarioFactory) {
+        return deploymentScenarioFactory.getWorkbenchWithKieServerScenarioBuilder().build();
+    }
+
+    @Before
+    public void setUp() {
+        WorkbenchUtils.deployProjectToWorkbench(gitProvider, deploymentScenario.getWorkbenchDeployment(), PROJECT_NAME);
+
+        kieServerMgmtControllerClient = KieServerControllerClientProvider.getKieServerMgmtControllerClient(deploymentScenario.getWorkbenchDeployment());
+
+        kieServicesClient = KieServerClientProvider.getKieServerClient(deploymentScenario.getKieServerDeployment());
+        processServicesClient = KieServerClientProvider.getProcessClient(deploymentScenario.getKieServerDeployment());
+        queryServicesClient = KieServerClientProvider.getQueryClient(deploymentScenario.getKieServerDeployment());
+    }
+
+    @Test
+    public void kieServerScaleTest() {
+        KieServerInfo serverInfo = kieServicesClient.getServerInfo().getResult();
+        checkServerTemplateIsRegistred(serverInfo.getServerId(), serverInfo.getName());
+
+        scaleKieServerTo(0);
+        checkServerTemplateInstanceCount(serverInfo.getServerId(), 0);
+
+        scaleKieServerTo(1);
+        checkServerTemplateInstanceCount(serverInfo.getServerId(), 1);
+
+        logger.debug("Register Kie Container to Kie Server");
+        kieServerMgmtControllerClient.saveContainerSpec(serverInfo.getServerId(), serverInfo.getName(), CONTAINER_ID, CONTAINER_ALIAS, PROJECT_GROUP_ID, PROJECT_NAME, PROJECT_VERSION, KieContainerStatus.STARTED);
+        KieServerClientProvider.waitForContainerStart(deploymentScenario.getKieServerDeployment(), CONTAINER_ID);
+
+        logger.debug("Start process instance");
+        Long signalPid = processServicesClient.startProcess(CONTAINER_ID, SIGNALTASK_PROCESS_ID);
+        assertThat(signalPid).isNotNull().isGreaterThan(0L);
+        assertThat(queryServicesClient.findProcessInstances(0, 10)).isNotNull().hasSize(1);
+
+        scaleKieServerTo(0);
+        checkServerTemplateInstanceCount(serverInfo.getServerId(), 0);
+
+        logger.debug("Send REST request to Kie Server");
+        sendRESTCallsToUnavailableKieServer();
+
+        scaleKieServerTo(1);
+        checkServerTemplateInstanceCount(serverInfo.getServerId(), 1);
+
+        logger.debug("Start new process instance");
+        Long newPid = processServicesClient.startProcess(CONTAINER_ID, SIGNALTASK_PROCESS_ID);
+        assertThat(newPid).isNotNull().isGreaterThan(0L);
+
+        logger.debug("Check started processes");
+        assertThat(queryServicesClient.findProcessInstances(0, 10)).isNotNull().hasSize(2);
+
+        ProcessInstance processInstance = processServicesClient.getProcessInstance(CONTAINER_ID, signalPid);
+        assertThat(processInstance).isNotNull();
+        assertThat(processInstance.getState()).isNotNull().isEqualTo(org.kie.api.runtime.process.ProcessInstance.STATE_ACTIVE);
+    }
+
+    private void scaleKieServerTo(int count) {
+        logger.debug("Scale Kie Server to " + count);
+        deploymentScenario.getKieServerDeployment().scale(count);
+        deploymentScenario.getKieServerDeployment().waitForScale();
+    }
+
+    protected void checkServerTemplateIsRegistred(String expectedId, String expectedName) {
+        assertThat(kieServerMgmtControllerClient.listServerTemplates()).hasSize(1);
+        ServerTemplate serverTemplate = kieServerMgmtControllerClient.listServerTemplates().iterator().next();
+        assertThat(serverTemplate.getId()).isEqualTo(expectedId);
+        assertThat(serverTemplate.getName()).isEqualTo(expectedName);
+    }
+
+    protected void checkServerTemplateInstanceCount(String serverTemplateId, int expected) {
+        ServerTemplate serverTemplate = kieServerMgmtControllerClient.getServerTemplate(serverTemplateId);
+        assertThat(serverTemplate).isNotNull();
+        assertThat(serverTemplate.getServerInstanceKeys()).isNotNull().hasSize(expected);
+    }
+
+    private void sendRESTCallsToUnavailableKieServer() {
+        Client httpKieServerClient = new ResteasyClientBuilder()
+                .establishConnectionTimeout(10, TimeUnit.SECONDS)
+                .socketTimeout(10, TimeUnit.SECONDS)
+                .register(new Authenticator(deploymentScenario.getKieServerDeployment().getUsername(),
+                                deploymentScenario.getKieServerDeployment().getPassword()))
+                .build();
+
+        WebTarget target = httpKieServerClient.target(deploymentScenario.getKieServerDeployment().getUrl().toString() + GET_CONTAINERS_REST_ENDPOINT);
+        Response response = target.request().get();
+        response.close();
+        assertThat(response.getStatus()).isEqualTo(Response.Status.SERVICE_UNAVAILABLE.getStatusCode());
+    }
+
+}
