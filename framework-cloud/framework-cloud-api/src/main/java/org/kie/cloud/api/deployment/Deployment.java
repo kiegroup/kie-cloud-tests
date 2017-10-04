d3509519b013bc7aaae5cd143e862543efebb833
Jakub Schwan <jschwan@redhat.com>
2017-10-04 15:28:00 +0200

    [GUVNOR-3534] Drools session failover

--- framework-cloud/framework-cloud-api/src/main/java/org/kie/cloud/api/deployment/Deployment.java
+++ framework-cloud/framework-cloud-api/src/main/java/org/kie/cloud/api/deployment/Deployment.java
@@ -15,6 +15,7 @@
 
 package org.kie.cloud.api.deployment;
 
+import java.time.Duration;
 import java.util.List;
 
 /**
@@ -75,4 +76,10 @@ public interface Deployment {
      */
     boolean isReady();
 
+    /**
+     * This method set a router timeout for the cloud deployment.
+     *
+     * @param timeoutValue Timeout value.
+     */
+    void setRouterTimeout(Duration timeoutValue);
 }
