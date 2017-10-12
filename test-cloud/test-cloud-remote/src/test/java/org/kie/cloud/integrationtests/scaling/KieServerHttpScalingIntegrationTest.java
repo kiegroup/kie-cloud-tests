accf0039ea74864950056a78741a87cf49b1e155
Karel Suta <ksuta@redhat.com>
2017-10-12 12:42:22 +0200

    GUVNOR-3506: Kie server HTTP scaling test

---
+++ test-cloud/test-cloud-remote/src/test/java/org/kie/cloud/integrationtests/scaling/KieServerHttpScalingIntegrationTest.java
@@ -0,0 +1,120 @@
+/*
+ * Copyright 2017 JBoss by Red Hat.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kie.cloud.integrationtests.scaling;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.kie.cloud.api.DeploymentScenarioBuilderFactory;
+import org.kie.cloud.api.scenario.WorkbenchWithKieServerScenario;
+import org.kie.cloud.common.provider.KieServerClientProvider;
+import org.kie.cloud.common.provider.KieServerControllerClientProvider;
+import org.kie.cloud.integrationtests.AbstractCloudIntegrationTest;
+import org.kie.cloud.maven.MavenDeployer;
+import org.kie.cloud.maven.constants.MavenConstants;
+import org.kie.server.api.model.KieContainerResource;
+import org.kie.server.api.model.KieContainerResourceList;
+import org.kie.server.api.model.KieContainerStatus;
+import org.kie.server.api.model.KieServerInfo;
+import org.kie.server.api.model.ServiceResponse;
+import org.kie.server.client.KieServicesClient;
+import org.kie.server.controller.api.model.runtime.ServerInstanceKey;
+import org.kie.server.controller.api.model.spec.ContainerSpec;
+import org.kie.server.integrationtests.controller.client.KieServerMgmtControllerClient;
+
+public class KieServerHttpScalingIntegrationTest extends AbstractCloudIntegrationTest<WorkbenchWithKieServerScenario> {
+
+    private KieServerMgmtControllerClient kieControllerClient;
+    private KieServicesClient kieServerClient;
+
+    @Override
+    protected WorkbenchWithKieServerScenario createDeploymentScenario(DeploymentScenarioBuilderFactory deploymentScenarioFactory) {
+        return deploymentScenarioFactory.getWorkbenchWithKieServerScenarioBuilder()
+                .withExternalMavenRepo(MavenConstants.getMavenRepoUrl(), MavenConstants.getMavenRepoUser(), MavenConstants.getMavenRepoPassword())
+                .build();
+    }
+
+    @Before
+    public void setUp() {
+        MavenDeployer.buildAndDeployMavenProject(ClassLoader.class.getResource("/kjars-sources/definition-project-snapshot").getFile());
+
+        kieControllerClient = KieServerControllerClientProvider.getKieServerMgmtControllerClient(deploymentScenario.getWorkbenchDeployment());
+        kieServerClient = KieServerClientProvider.getKieServerClient(deploymentScenario.getKieServerDeployment());
+    }
+
+    @Test
+    public void testConnectionBetweenDeployables() {
+        String kieServerId = getKieServerId(kieServerClient);
+
+        verifyServerTemplateContainsKieServers(kieServerId, 1);
+
+        deployAndStartContainer();
+
+        verifyContainerIsDeployed(kieServerClient, CONTAINER_ID);
+        verifyServerTemplateContainsContainer(kieServerId, CONTAINER_ID);
+
+        scaleKieServerTo(3);
+
+        verifyServerTemplateContainsKieServers(kieServerId, 3);
+
+        scaleKieServerTo(1);
+
+        verifyServerTemplateContainsKieServers(kieServerId, 1);
+    }
+
+    private String getKieServerId(KieServicesClient kieServerClient) {
+        ServiceResponse<KieServerInfo> serverInfo = kieServerClient.getServerInfo();
+        assertThat(serverInfo.getType()).isEqualTo(ServiceResponse.ResponseType.SUCCESS);
+
+        return serverInfo.getResult().getServerId();
+    }
+
+    private void verifyContainerIsDeployed(KieServicesClient kieServerClient, String containerId) {
+        ServiceResponse<KieContainerResourceList> containers = kieServerClient.listContainers();
+        assertThat(containers.getType()).isEqualTo(ServiceResponse.ResponseType.SUCCESS);
+
+        List<KieContainerResource> containerList = containers.getResult().getContainers();
+        assertThat(containerList).hasSize(1);
+        assertThat(containerList.get(0).getContainerId()).isEqualTo(containerId);
+        assertThat(containerList.get(0).getStatus()).isEqualTo(KieContainerStatus.STARTED);
+    }
+
+    private void verifyServerTemplateContainsContainer(String serverTemplate, String containerId) {
+        Collection<ContainerSpec> containersSpec = kieControllerClient.getServerTemplate(serverTemplate).getContainersSpec();
+        assertThat(containersSpec).hasSize(1);
+        assertThat(containersSpec.iterator().next().getId()).isEqualTo(containerId);
+    }
+
+    private void verifyServerTemplateContainsKieServers(String serverTemplate, int numberOfKieServers) {
+        Collection<ServerInstanceKey> kieServers = kieControllerClient.getServerTemplate(serverTemplate).getServerInstanceKeys();
+        assertThat(kieServers).hasSize(numberOfKieServers);
+    }
+
+    private void deployAndStartContainer() {
+        KieServerInfo serverInfo = kieServerClient.getServerInfo().getResult();
+        kieControllerClient.saveContainerSpec(serverInfo.getServerId(), serverInfo.getName(), CONTAINER_ID, CONTAINER_ALIAS, PROJECT_GROUP_ID, DEFINITION_PROJECT_SNAPSHOT_NAME, DEFINITION_PROJECT_SNAPSHOT_VERSION, KieContainerStatus.STARTED);
+        KieServerClientProvider.waitForContainerStart(deploymentScenario.getKieServerDeployment(), CONTAINER_ID);
+    }
+
+    private void scaleKieServerTo(int count) {
+        deploymentScenario.getKieServerDeployment().scale(count);
+        deploymentScenario.getKieServerDeployment().waitForScale();
+    }
+}
