8f21129431f7ed49709a470a15321feeb8d58188
Jakub Schwan <jschwan@redhat.com>
2018-10-12 17:01:51 +0200

    Add more tests with Ldap auth

--- test-cloud/test-cloud-remote/src/test/java/org/kie/cloud/integrationtests/ldap/KieServerWithMySqlLdapIntegrationTest.java
+++ test-cloud/test-cloud-remote/src/test/java/org/kie/cloud/integrationtests/ldap/KieServerWithMySqlLdapIntegrationTest.java
@@ -1,9 +1,8 @@
 /*
- * Copyright 2018 JBoss by Red Hat.
+ * Copyright 2018 Red Hat, Inc. and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -12,31 +11,29 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
+*/
+
 package org.kie.cloud.integrationtests.ldap;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
-import org.kie.cloud.api.scenario.WorkbenchKieServerScenario;
+import org.kie.cloud.api.scenario.KieServerWithDatabaseScenario;
 import org.kie.cloud.api.settings.LdapSettings;
 import org.kie.cloud.integrationtests.AbstractCloudIntegrationTest;
 import org.kie.cloud.integrationtests.category.JBPMOnly;
 import org.kie.cloud.integrationtests.testproviders.FireRulesTestProvider;
 import org.kie.cloud.integrationtests.testproviders.HttpsKieServerTestProvider;
-import org.kie.cloud.integrationtests.testproviders.HttpsWorkbenchTestProvider;
 import org.kie.cloud.integrationtests.testproviders.OptaplannerTestProvider;
-import org.kie.cloud.integrationtests.testproviders.PersistenceTestProvider;
 import org.kie.cloud.integrationtests.testproviders.ProcessTestProvider;
 import org.kie.cloud.integrationtests.util.LdapSettingsConstants;
 import org.kie.cloud.integrationtests.util.ScenarioDeployer;
 import org.kie.cloud.maven.constants.MavenConstants;
 
-public class AuthLdapIntegrationTest extends AbstractCloudIntegrationTest {
+public class KieServerWithMySqlLdapIntegrationTest extends AbstractCloudIntegrationTest {
 
-    private static WorkbenchKieServerScenario deploymentScenario;
+    private static KieServerWithDatabaseScenario deploymentScenario;
 
     @BeforeClass
     public static void initializeDeployment() {
@@ -52,26 +49,19 @@ public class AuthLdapIntegrationTest extends AbstractCloudIntegrationTest {
                 .withLdapRolesCtxDn(LdapSettingsConstants.ROLES_CTX_DN)
                 .withLdapRoleFilter(LdapSettingsConstants.ROLE_FILTER)
                 .withLdapRoleRecursion(LdapSettingsConstants.ROLE_RECURSION)
-                .withLdapDefaultRole(LdapSettingsConstants.DEFAULT_ROLE)
-                .build();
+                .withLdapDefaultRole(LdapSettingsConstants.DEFAULT_ROLE).build();
 
-        deploymentScenario = deploymentScenarioFactory.getWorkbenchKieServerPersistentScenarioBuilder()
-                .withExternalMavenRepo(MavenConstants.getMavenRepoUrl(), MavenConstants.getMavenRepoUser(), MavenConstants.getMavenRepoPassword())
-                .withLdapSettings(ldapSettings)
+        deploymentScenario = deploymentScenarioFactory.getKieServerWithMySqlScenarioBuilder()
+                .withLdapSettings(ldapSettings).withExternalMavenRepo(MavenConstants.getMavenRepoUrl(),
+                        MavenConstants.getMavenRepoUser(), MavenConstants.getMavenRepoPassword())
                 .build();
-        deploymentScenario.setLogFolderName(AuthLdapIntegrationTest.class.getSimpleName());
+        deploymentScenario.setLogFolderName(KieServerWithMySqlLdapIntegrationTest.class.getSimpleName());
         ScenarioDeployer.deployScenario(deploymentScenario);
     }
 
     @AfterClass
     public static void cleanEnvironment() {
         ScenarioDeployer.undeployScenario(deploymentScenario);
-    }
-
-    @Test
-    @Ignore("Ignored as the tests are affected by RHPAM-1354. Unignore when the JIRA will be fixed. https://issues.jboss.org/browse/RHPAM-1354")
-    public void testWorkbenchControllerPersistence() {
-        PersistenceTestProvider.testControllerPersistence(deploymentScenario);
     }
 
     @Test
@@ -91,19 +81,8 @@ public class AuthLdapIntegrationTest extends AbstractCloudIntegrationTest {
     }
 
     @Test
-    public void testDeployContainerFromWorkbench() {
-        FireRulesTestProvider.testDeployFromWorkbenchAndFireRules(deploymentScenario.getWorkbenchDeployment(), deploymentScenario.getKieServerDeployment());
-    }
-
-    @Test
     public void testKieServerHttps() {
         HttpsKieServerTestProvider.testKieServerInfo(deploymentScenario.getKieServerDeployment(), false);
         HttpsKieServerTestProvider.testDeployContainer(deploymentScenario.getKieServerDeployment(), false);
-    }
-
-    @Test
-    public void testWorkbenchHttps() {
-        HttpsWorkbenchTestProvider.testLoginScreen(deploymentScenario.getWorkbenchDeployment(), false);
-        HttpsWorkbenchTestProvider.testControllerOperations(deploymentScenario.getWorkbenchDeployment(), false);
     }
 }
---
+++ test-cloud/test-cloud-remote/src/test/java/org/kie/cloud/integrationtests/ldap/WorkbenchRuntimeSmartRouterTwoKieServersTwoDatabasesScenarioLdapIntegrationTest.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright 2018 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+package org.kie.cloud.integrationtests.ldap;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.kie.cloud.api.deployment.KieServerDeployment;
+import org.kie.cloud.api.deployment.WorkbenchDeployment;
+import org.kie.cloud.api.scenario.WorkbenchRuntimeSmartRouterTwoKieServersTwoDatabasesScenario;
+import org.kie.cloud.api.settings.LdapSettings;
+import org.kie.cloud.integrationtests.AbstractCloudIntegrationTest;
+import org.kie.cloud.integrationtests.category.JBPMOnly;
+import org.kie.cloud.integrationtests.testproviders.FireRulesTestProvider;
+import org.kie.cloud.integrationtests.testproviders.HttpsKieServerTestProvider;
+import org.kie.cloud.integrationtests.testproviders.HttpsWorkbenchTestProvider;
+import org.kie.cloud.integrationtests.testproviders.OptaplannerTestProvider;
+import org.kie.cloud.integrationtests.testproviders.ProcessTestProvider;
+import org.kie.cloud.integrationtests.util.LdapSettingsConstants;
+import org.kie.cloud.integrationtests.util.ScenarioDeployer;
+import org.kie.cloud.maven.constants.MavenConstants;
+
+public class WorkbenchRuntimeSmartRouterTwoKieServersTwoDatabasesScenarioLdapIntegrationTest extends AbstractCloudIntegrationTest {
+
+    private static WorkbenchRuntimeSmartRouterTwoKieServersTwoDatabasesScenario deploymentScenario;
+
+    @BeforeClass
+    public static void initializeDeployment() {
+
+        LdapSettings ldapSettings = deploymentScenarioFactory.getLdapSettingsBuilder()
+                .withLdapBindDn(LdapSettingsConstants.BIND_DN)
+                .withLdapBindCredential(LdapSettingsConstants.BIND_CREDENTIAL)
+                .withLdapBaseCtxDn(LdapSettingsConstants.BASE_CTX_DN)
+                .withLdapBaseFilter(LdapSettingsConstants.BASE_FILTER)
+                .withLdapSearchScope(LdapSettingsConstants.SEARCH_SCOPE)
+                .withLdapSearchTimeLimit(LdapSettingsConstants.SEARCH_TIME_LIMIT)
+                .withLdapRoleAttributeId(LdapSettingsConstants.ROLE_ATTRIBUTE_ID)
+                .withLdapRolesCtxDn(LdapSettingsConstants.ROLES_CTX_DN)
+                .withLdapRoleFilter(LdapSettingsConstants.ROLE_FILTER)
+                .withLdapRoleRecursion(LdapSettingsConstants.ROLE_RECURSION)
+                .withLdapDefaultRole(LdapSettingsConstants.DEFAULT_ROLE)
+                .build();
+
+        deploymentScenario = deploymentScenarioFactory.getWorkbenchRuntimeSmartRouterTwoKieServersTwoDatabasesScenarioBuilder()
+        .withLdapSettings(ldapSettings)
+                .withExternalMavenRepo(MavenConstants.getMavenRepoUrl(), MavenConstants.getMavenRepoUser(), MavenConstants.getMavenRepoPassword())
+                .build();
+        deploymentScenario.setLogFolderName(WorkbenchRuntimeSmartRouterTwoKieServersTwoDatabasesScenarioLdapIntegrationTest.class.getSimpleName());
+        ScenarioDeployer.deployScenario(deploymentScenario);
+    }
+
+    @AfterClass
+    public static void cleanEnvironment() {
+        ScenarioDeployer.undeployScenario(deploymentScenario);
+    }
+
+    @Test
+    public void testRulesFromExternalMavenRepo() {
+        FireRulesTestProvider.testFireRules(deploymentScenario.getKieServerOneDeployment());
+        FireRulesTestProvider.testFireRules(deploymentScenario.getKieServerTwoDeployment());
+    }
+
+    @Test
+    @Category(JBPMOnly.class)
+    public void testProcessFromExternalMavenRepo() {
+        ProcessTestProvider.testProcesses(deploymentScenario.getKieServerOneDeployment());
+        ProcessTestProvider.testProcesses(deploymentScenario.getKieServerTwoDeployment());
+    }
+
+    @Test
+    public void testSolverFromExternalMavenRepo() {
+        OptaplannerTestProvider.testExecuteSolver(deploymentScenario.getKieServerOneDeployment());
+        OptaplannerTestProvider.testExecuteSolver(deploymentScenario.getKieServerTwoDeployment());
+    }
+
+    @Test
+    public void testKieServerHttps() {
+        for (KieServerDeployment kieServerDeployment : deploymentScenario.getKieServerDeployments()) {
+            HttpsKieServerTestProvider.testKieServerInfo(kieServerDeployment, false);
+            HttpsKieServerTestProvider.testDeployContainer(kieServerDeployment, false);
+        }
+    }
+
+    @Test
+    public void testWorkbenchHttps() {
+        for (WorkbenchDeployment workbenchDeployment : deploymentScenario.getWorkbenchDeployments()) {
+            HttpsWorkbenchTestProvider.testLoginScreen(workbenchDeployment, false);
+            HttpsWorkbenchTestProvider.testControllerOperations(workbenchDeployment, false);
+        }
+    }
+}
