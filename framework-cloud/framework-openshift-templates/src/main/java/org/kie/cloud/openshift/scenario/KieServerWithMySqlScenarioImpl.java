a936b932f72f02f47a45c2c96f471053343fee49
Karel Suta <ksuta@redhat.com>
2018-12-05 12:29:28 +0100

    KIECLOUD-48: Kill Kie server pods when database completes startup

--- framework-cloud/framework-openshift-templates/src/main/java/org/kie/cloud/openshift/scenario/KieServerWithMySqlScenarioImpl.java
+++ framework-cloud/framework-openshift-templates/src/main/java/org/kie/cloud/openshift/scenario/KieServerWithMySqlScenarioImpl.java
@@ -92,11 +92,14 @@ public class KieServerWithMySqlScenarioImpl extends OpenShiftScenario implements
 
         databaseDeployment = new DatabaseDeploymentImpl(project);
 
-        logger.info("Waiting for Kie server deployment to become ready.");
-        kieServerDeployment.waitForScale();
-
         logger.info("Waiting for Database deployment to become ready.");
         databaseDeployment.waitForScale();
+
+        // TODO: Workaround for KIECLOUD-48, respin Kie server when database is ready
+        kieServerDeployment.deleteInstances(kieServerDeployment.getInstances());
+
+        logger.info("Waiting for Kie server deployment to become ready.");
+        kieServerDeployment.waitForScale();
 
         logNodeNameOfAllInstances();
     }
