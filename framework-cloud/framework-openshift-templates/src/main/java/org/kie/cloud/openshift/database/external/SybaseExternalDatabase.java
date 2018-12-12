042b9ce23f3e8686e68ebfdf297130cad260e826
Karel Suta <ksuta@redhat.com>
2018-12-12 17:01:17 +0100

    Fix wrong template parameter for external DB
    Also remove URL parameter for PostgreSQL

--- framework-cloud/framework-openshift-templates/src/main/java/org/kie/cloud/openshift/database/external/SybaseExternalDatabase.java
+++ framework-cloud/framework-openshift-templates/src/main/java/org/kie/cloud/openshift/database/external/SybaseExternalDatabase.java
@@ -28,8 +28,8 @@ public class SybaseExternalDatabase extends AbstractSybaseExternalDatabase {
     public Map<String, String> getExternalDatabaseEnvironmentVariables() {
         Map<String, String> envVariables = new HashMap<>();
         envVariables.put(OpenShiftTemplateConstants.KIE_SERVER_EXTERNALDB_DRIVER, getDriverName());
-        envVariables.put(OpenShiftTemplateConstants.KIE_SERVER_EXTERNALDB_HOST, DeploymentConstants.getDatabaseHost());
-        envVariables.put(OpenShiftTemplateConstants.KIE_SERVER_EXTERNALDB_PORT, DeploymentConstants.getDatabasePort());
+        envVariables.put(OpenShiftTemplateConstants.KIE_SERVER_EXTERNALDB_SERVICE_HOST, DeploymentConstants.getDatabaseHost());
+        envVariables.put(OpenShiftTemplateConstants.KIE_SERVER_EXTERNALDB_SERVICE_PORT, DeploymentConstants.getDatabasePort());
         envVariables.put(OpenShiftTemplateConstants.KIE_SERVER_EXTERNALDB_DB, DeploymentConstants.getExternalDatabaseName());
         envVariables.put(OpenShiftTemplateConstants.KIE_SERVER_EXTERNALDB_USER, DeploymentConstants.getDatabaseUsername());
         envVariables.put(OpenShiftTemplateConstants.KIE_SERVER_EXTERNALDB_PWD, DeploymentConstants.getDatabasePassword());
