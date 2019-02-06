37bd7fca86c1c6530d9cf93387bb9c62e61c638e
Karel Suta <krlsuta@gmail.com>
2019-02-06 09:46:40 +0100

    Update framework-cloud/framework-openshift-apb/src/main/java/org/kie/cloud/openshift/scenario/builder/ClusteredWorkbenchRuntimeSmartRouterTwoKieServersTwoDatabasesScenarioBuilderApb.java
    Co-Authored-By: jakubschwan <jakubschwan@users.noreply.github.com>

--- framework-cloud/framework-openshift-apb/src/main/java/org/kie/cloud/openshift/scenario/builder/ClusteredWorkbenchRuntimeSmartRouterTwoKieServersTwoDatabasesScenarioBuilderApb.java
+++ framework-cloud/framework-openshift-apb/src/main/java/org/kie/cloud/openshift/scenario/builder/ClusteredWorkbenchRuntimeSmartRouterTwoKieServersTwoDatabasesScenarioBuilderApb.java
@@ -38,7 +38,7 @@ public class ClusteredWorkbenchRuntimeSmartRouterTwoKieServersTwoDatabasesScenar
     private boolean deploySSO = false;
 
     public ClusteredWorkbenchRuntimeSmartRouterTwoKieServersTwoDatabasesScenarioBuilderApb() {
-        // Required values to create persitence values.
+        // Required values to create persistence values.
         extraVars.put(OpenShiftApbConstants.APB_PLAN_ID, ApbConstants.Plans.MANAGED);
         extraVars.put(OpenShiftApbConstants.APB_KIESERVER_DB_TYPE, ApbConstants.DbType.MYSQL);
         extraVars.put(OpenShiftApbConstants.APB_IMAGE_STREAM_TAG,OpenShiftConstants.getApbImageStreamTag());
