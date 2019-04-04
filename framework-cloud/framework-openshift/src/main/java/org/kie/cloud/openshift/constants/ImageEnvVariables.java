4d420b9d4bde67a36b620f0ed727df639836dc9c
Karel Suta <ksuta@redhat.com>
2019-04-04 20:56:33 +0200

    Add missing configuration to Operator scenarios
    Except for LDAP and SSO

--- framework-cloud/framework-openshift/src/main/java/org/kie/cloud/openshift/constants/ImageEnvVariables.java
+++ framework-cloud/framework-openshift/src/main/java/org/kie/cloud/openshift/constants/ImageEnvVariables.java
@@ -21,10 +21,25 @@ public class ImageEnvVariables {
     public static final String KIE_SERVER_PWD = "KIE_SERVER_PWD";
     public static final String KIE_ADMIN_USER = "KIE_ADMIN_USER";
     public static final String KIE_ADMIN_PWD = "KIE_ADMIN_PWD";
+    public static final String KIE_MAVEN_USER = "KIE_MAVEN_USER";
+    public static final String KIE_MAVEN_PWD = "KIE_MAVEN_PWD";
 
+    public static final String KIE_SERVER_ID = "KIE_SERVER_ID";
     public static final String KIE_SERVER_MODE = "KIE_SERVER_MODE";
+
+    public static final String KIE_SERVER_ROUTER_ID = "KIE_SERVER_ROUTER_ID";
 
     public static final String EXTERNAL_MAVEN_REPO_URL = "EXTERNAL_MAVEN_REPO_URL";
     public static final String EXTERNAL_MAVEN_REPO_USERNAME = "EXTERNAL_MAVEN_REPO_USERNAME";
     public static final String EXTERNAL_MAVEN_REPO_PASSWORD = "EXTERNAL_MAVEN_REPO_PASSWORD";
+
+    public static final String TIMER_SERVICE_DATA_STORE_REFRESH_INTERVAL = "TIMER_SERVICE_DATA_STORE_REFRESH_INTERVAL";
+
+    public static final String HOSTNAME_HTTP = "HOSTNAME_HTTP";
+    public static final String HOSTNAME_HTTPS = "HOSTNAME_HTTPS";
+
+    public static final String RHDMCENTR_MAVEN_REPO_USERNAME = "RHDMCENTR_MAVEN_REPO_USERNAME";
+    public static final String RHDMCENTR_MAVEN_REPO_PASSWORD = "RHDMCENTR_MAVEN_REPO_PASSWORD";
+    public static final String RHPAMCENTR_MAVEN_REPO_USERNAME = "RHPAMCENTR_MAVEN_REPO_USERNAME";
+    public static final String RHPAMCENTR_MAVEN_REPO_PASSWORD = "RHPAMCENTR_MAVEN_REPO_PASSWORD";
 }
