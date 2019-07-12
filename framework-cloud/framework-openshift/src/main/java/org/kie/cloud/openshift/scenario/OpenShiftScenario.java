3cea2caacc012f8b959e7e1475f081caf780d443
Tristan Radisson <tradisso@redhat.com>
2019-07-12 09:07:24 +0200

    BAQE-904: Store logs for pods that fail to deploy (#413)
    BAQE-904: Setup parallel thread(s) to log the pods
    
    On scenario deployment, a thread is started (with a 5s repeating
    schedule) to get the different started pods of a project and put an
    observable thread on each of them.
    On scenario undeploy, all created threads are stopped, if not already
    done.
    
    Other:
    * Stop all observing threads when scenario is over
    * Flush logs from pods still running, to be sure we get the last logs
    * Use executor shutdown functionality
    * Added test for InstancesLogCollectorRunnable
    * Added synchronization on iterating the observed instances to avoid
    ConcurrentModificationException
    
    Signed-off-by: tradisso <tradisso@tradisso.remote.csb>

--- framework-cloud/framework-openshift/src/main/java/org/kie/cloud/openshift/scenario/OpenShiftScenario.java
+++ framework-cloud/framework-openshift/src/main/java/org/kie/cloud/openshift/scenario/OpenShiftScenario.java
@@ -19,15 +19,18 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import io.fabric8.kubernetes.api.model.Pod;
 import org.kie.cloud.api.deployment.Deployment;
 import org.kie.cloud.api.scenario.DeploymentScenario;
 import org.kie.cloud.api.scenario.DeploymentScenarioListener;
-import org.kie.cloud.common.logs.InstanceLogUtil;
 import org.kie.cloud.openshift.OpenShiftController;
 import org.kie.cloud.openshift.constants.OpenShiftConstants;
 import org.kie.cloud.openshift.constants.images.imagestream.ImageStreamProvider;
+import org.kie.cloud.openshift.log.InstancesLogCollectorRunnable;
 import org.kie.cloud.openshift.resource.Project;
 import org.kie.cloud.openshift.template.OpenShiftTemplate;
 import org.slf4j.Logger;
@@ -35,10 +38,15 @@ import org.slf4j.LoggerFactory;
 
 public abstract class OpenShiftScenario<T extends DeploymentScenario<T>> implements DeploymentScenario<T> {
 
+    private static final Integer DEFAULT_SCHEDULED_FIX_RATE_LOG_COLLECTOR_IN_SECONDS = 5;
+
     protected String projectName;
     protected Project project;
     private String logFolderName;
     private boolean createImageStreams;
+
+    private ScheduledExecutorService logCollectorExecutorService;
+    private InstancesLogCollectorRunnable instancesLogCollectorRunnable;
 
     private List<DeploymentScenarioListener<T>> deploymentScenarioListeners = new ArrayList<>();
 
@@ -81,6 +89,12 @@ public abstract class OpenShiftScenario<T extends DeploymentScenario<T>> impleme
         logger.info("Creating project " + projectName);
         project = OpenShiftController.createProject(projectName);
 
+        // Init the log collector
+        logger.info("Launch instances log collector on project {}", projectName);
+        logCollectorExecutorService = Executors.newScheduledThreadPool(1);
+        instancesLogCollectorRunnable = new InstancesLogCollectorRunnable(project, getLogFolderName());
+        logCollectorExecutorService.scheduleWithFixedDelay(instancesLogCollectorRunnable, 0, DEFAULT_SCHEDULED_FIX_RATE_LOG_COLLECTOR_IN_SECONDS, TimeUnit.SECONDS);
+
         logger.info("Creating generally used secret from " + OpenShiftTemplate.SECRET.getTemplateUrl().toString());
         project.processTemplateAndCreateResources(OpenShiftTemplate.SECRET.getTemplateUrl(), Collections.singletonMap(OpenShiftConstants.SECRET_NAME, OpenShiftConstants.getKieApplicationSecretName()));
 
@@ -104,11 +118,19 @@ public abstract class OpenShiftScenario<T extends DeploymentScenario<T>> impleme
     @Override
     public void undeploy() {
         try {
-            InstanceLogUtil.writeDeploymentLogs(this);
+            logger.info("Release log collector(s)");
+            try {
+                logCollectorExecutorService.shutdownNow();
+                logCollectorExecutorService = null;
+                instancesLogCollectorRunnable.closeAndFlushRemainingInstanceCollectors(5000);
+            } catch (Exception e) {
+                logger.error("Error killing log collector thread", e);
+            }
 
             project.delete();
             project.close();
         } catch (Exception e) {
+            logger.error("Error undeploy", e);
             throw new RuntimeException("Error while undeploying scenario.", e);
         }
     }
