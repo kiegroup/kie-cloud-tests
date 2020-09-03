9b45dd3b94c83d1fe2f2075f2613a51383d3280e
Jakub Schwan <jschwan@redhat.com>
2020-09-03 17:24:34 +0200

    Revert "BAQE-1519: Fail fast when OC command is not installed (#671)" (#673)
    This reverts commit f2578954961868c5a0e2952ba38054fc33d8b380.

--- framework-cloud/framework-openshift/src/main/java/org/kie/cloud/openshift/constants/OpenShiftConstants.java
+++ framework-cloud/framework-openshift/src/main/java/org/kie/cloud/openshift/constants/OpenShiftConstants.java
@@ -19,7 +19,6 @@ import java.io.File;
 import java.util.Optional;
 
 import cz.xtf.core.config.OpenShiftConfig;
-import cz.xtf.core.openshift.OpenShifts;
 import org.kie.cloud.api.constants.Constants;
 
 public class OpenShiftConstants implements Constants {
@@ -265,9 +264,5 @@ public class OpenShiftConstants implements Constants {
         System.setProperty(OpenShiftConfig.OPENSHIFT_ADMIN_PASSWORD, getOpenShiftAdminPassword() != null ? getOpenShiftAdminPassword() : getOpenShiftPassword());
         System.setProperty(OpenShiftConfig.OPENSHIFT_VERSION, getOpenShiftVersion());
         System.setProperty(OpenShiftConfig.OPENSHIFT_NAMESPACE, "openshift");
-
-        if (OpenShifts.adminBinary() == null) {
-            throw new RuntimeException("Oc client is not installed");
-        }
     }
 }
