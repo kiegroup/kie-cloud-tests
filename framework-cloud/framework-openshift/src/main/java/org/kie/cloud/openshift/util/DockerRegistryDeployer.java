4de96e452435380a02daa6354014d2c9caf54c02
Jakub Schwan <jschwan@redhat.com>
2021-06-17 18:41:11 +0200

    Create registry using mirrored image stream

--- framework-cloud/framework-openshift/src/main/java/org/kie/cloud/openshift/util/DockerRegistryDeployer.java
+++ framework-cloud/framework-openshift/src/main/java/org/kie/cloud/openshift/util/DockerRegistryDeployer.java
@@ -15,9 +15,16 @@
  */
 package org.kie.cloud.openshift.util;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
 import cz.xtf.core.openshift.OpenShiftBinary;
 import cz.xtf.core.openshift.OpenShifts;
 import org.kie.cloud.api.deployment.DockerDeployment;
+import org.kie.cloud.openshift.constants.OpenShiftConstants;
+import org.kie.cloud.openshift.constants.images.imagestream.ImageStreamProvider;
 import org.kie.cloud.openshift.deployment.DockerDeploymentImpl;
 import org.kie.cloud.openshift.resource.Project;
 import org.slf4j.Logger;
@@ -29,6 +36,9 @@ import org.slf4j.LoggerFactory;
 public class DockerRegistryDeployer {
 
     private static final Logger logger = LoggerFactory.getLogger(DockerRegistryDeployer.class);
+
+    private static final String REGISTRY_IMAGE_STREAM_NAME = "registry-mirror";
+    private static final String REGISTRY_DEPLOYMENT_NAME = "registry";
 
     public static DockerDeployment deploy(Project project) {
         deployDockerRegistry(project);
@@ -46,7 +56,40 @@ public class DockerRegistryDeployer {
         // Login is part of binary retrieval
         OpenShiftBinary masterBinary = OpenShifts.masterBinary();
         masterBinary.project(project.getName());
-        masterBinary.execute("new-app", "registry:2", "-l", "deploymentConfig=registry");
-        masterBinary.execute("expose", "service", "registry");
+
+        String registryMirrorImageStream = OpenShiftConstants.getRegistryMirrorImageStream();
+        if (registryMirrorImageStream != null && !registryMirrorImageStream.isEmpty()) {
+            logger.info("Mirrored Registry docker image is provided.");
+            ImageStreamProvider.createImagesFromImageStreamFile(project, registryMirrorImageStream);
+            ImageStreamProvider.waitForImageStreamCreate(project, REGISTRY_IMAGE_STREAM_NAME);
+
+
+            logger.info("Creating new app using template and image from {} image stream.", REGISTRY_IMAGE_STREAM_NAME);
+            
+            logger.info("FOR DEBUG: {}", masterBinary.execute("new-app", "--image-stream="+REGISTRY_IMAGE_STREAM_NAME, "--name=registry", "--allow-missing-imagestream-tags", "-l", "deploymentConfig=registry", "-o", "yaml"));
+            project.processTemplateAndCreateResources(getRegistryTemplate(), geRegistryProperties());
+        } else {
+            logger.info("Mirrored Registry docker image is not provided.");
+            logger.info("Creating new app from docker image.");
+            masterBinary.execute("new-app", "registry:2", "-l", "deploymentConfig=registry");
+        }
+
+        masterBinary.execute("expose", "service", REGISTRY_DEPLOYMENT_NAME);
+    }
+
+    private static final String REGISTRY_TEMPLATE = "/deployments/registry.yaml";
+
+    private static URL getRegistryTemplate() {
+        try {
+            return new URL("file://" + MavenRepositoryDeployer.class.getResource(REGISTRY_TEMPLATE).getFile());
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("Wrong Registry template location", e);
+        }
+    }
+
+    private static Map<String, String> geRegistryProperties() {
+        Map<String, String> properties = new HashMap<>();
+        properties.put("APPLICATION_NAME", REGISTRY_DEPLOYMENT_NAME);
+        return properties;
     }
 }
