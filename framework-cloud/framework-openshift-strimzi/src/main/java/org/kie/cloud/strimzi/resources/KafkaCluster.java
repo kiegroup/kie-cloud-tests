562c4ead67c8a48cd232f62515b500d0e92768dd
Jiri Petrlik <jiripetrlik@gmail.com>
2021-09-09 11:29:40 +0200

    [BAQE-1956] Add missing metadata for Fabric client

--- framework-cloud/framework-openshift-strimzi/src/main/java/org/kie/cloud/strimzi/resources/KafkaCluster.java
+++ framework-cloud/framework-openshift-strimzi/src/main/java/org/kie/cloud/strimzi/resources/KafkaCluster.java
@@ -19,11 +19,15 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import io.fabric8.kubernetes.api.model.Namespaced;
 import io.fabric8.kubernetes.client.CustomResource;
 import io.fabric8.kubernetes.model.annotation.Group;
+import io.fabric8.kubernetes.model.annotation.Kind;
+import io.fabric8.kubernetes.model.annotation.Plural;
 import io.fabric8.kubernetes.model.annotation.Singular;
 import io.fabric8.kubernetes.model.annotation.Version;
 
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 @Group("kafka.strimzi.io")
+@Plural("kafkas")
+@Kind("Kafka")
 @Version("v1beta2")
 @Singular("kafka")
 public class KafkaCluster extends CustomResource<KafkaClusterSpec, KafkaClusterStatus> implements Namespaced {
